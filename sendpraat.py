cfb87349611d730adf450a8ae12e3423db9fbbf4
Rolando Muñoz <rolando.muar@gmail.com>
2020-08-24 03:51:16 -0500

    first version

--- sendpraat.py
+++ sendpraat.py
@@ -3,176 +3,86 @@ import os
 import re
 import time
 import platform
-import codecs
 
 class SendPraat():
   def __init__(self, sendpraat_dir):
-
+    self.init_variables(sendpraat_dir)
+    
+  def init_variables(self, sendpraat_dir):
     # Dir variables
-    self._cwd = os.getcwd()
-    self._sendpraat_dir = sendpraat_dir
-    self._transcriber_dir = os.path.join(self._cwd, 'transcriber-local')
-    self._logfile = os.path.join(self._transcriber_dir, 'log.txt')
-    self._audiofile = os.path.join(self._transcriber_dir, 'temp.wav')
+    self._CWD = os.getcwd()
+    self.SENDPRAAT_DIR = sendpraat_dir
+    self.SCRIPTS_DIR = os.path.join(self._CWD, 'praat-scripts')
+    self.LOCAL_DIR = os.path.join(self._CWD, 'transcriber-local')
+    self.LOGFILE_DIR = os.path.join(self.LOCAL_DIR, 'log.txt')
+    self.AUDIOFILE_DIR = os.path.join(self.LOCAL_DIR, 'temp.wav')
 
+    self.PUSH_TEXT = os.path.join(self.SCRIPTS_DIR, 'push_interval_text.praat')
+    self.PULL_TEXT = os.path.join(self.SCRIPTS_DIR, 'pull_interval_text.praat')
+    self.NEXT_TIER = os.path.join(self.SCRIPTS_DIR, 'next_tier.praat')
+    self.PREVIOUS_TIER = os.path.join(self.SCRIPTS_DIR, 'previous_tier.praat')
+    self.NEXT_INTERVAL = os.path.join(self.SCRIPTS_DIR, 'next_interval.praat')
+    self.PREVIOUS_INTERVAL = os.path.join(self.SCRIPTS_DIR, 'previous_interval.praat')
+    self.ZOOM_ALL = os.path.join(self.SCRIPTS_DIR, 'zoom_all.praat')
+    self.ZOOM_BACK = os.path.join(self.SCRIPTS_DIR, 'zoom_back.praat')
+    self.ZOOM_IN = os.path.join(self.SCRIPTS_DIR, 'zoom_in.praat')
+    self.ZOOM_OUT = os.path.join(self.SCRIPTS_DIR, 'zoom_out.praat')
+    self.ZOOM_TO_SELECTION = os.path.join(self.SCRIPTS_DIR, 'zoom_to_selection.praat')
+    self.EXTRACT_AUDIOFILE = os.path.join(self.SCRIPTS_DIR, 'extract_selected_audio.praat')
     # Create local dir
-    if not os.path.exists(self._transcriber_dir):
-      os.mkdir(self._transcriber_dir)
-
-    # Praat script pieces
-
-    self.sp_start = [self._sendpraat_dir, '0', 'praat']
+    if not os.path.exists(self.LOCAL_DIR):
+      os.mkdir(self.LOCAL_DIR)
+  
+    self.SENDPRAAT_CMD = [self.SENDPRAAT_DIR, '0', 'praat']
     if platform.system() == 'Windows':
-      self.sp_start.pop(1)
+      self.SENDPRAAT_CMD.pop(1)
 
-    self.sp_textgrideditor = self.sp_start + [
-      'tg = selected("TextGrid")',
-      'editor: tg'
-    ]
-
-    self.sp_write2log = [
-      'info$ = Editor info',
-      'text$ = nocheck Get label of interval',
-      'writeFile: "{}", info$ + "Text: " + text$'.format(self._logfile)
-    ]
-
-  def set_dir(self, sendpraat_dir):
-    self._sendpraat_dir = sendpraat_dir
-
+  def set_sendpraat_dir(self, sendpraat_dir):
+    self.SENDPRAAT_DIR = sendpraat_dir
+    
   def remove_log(self):
-    if os.path.exists(self._logfile):
-      os.remove(self._logfile)
+    if os.path.exists(self.LOGFILE_DIR):
+      os.remove(self.LOGFILE_DIR)
 
-  def remove_tempaudio(self):
-    if os.path.exists(self._audiofile):
-      os.remove(self._audiofile)
-
-  def play_sound(self, tmin, tmax):
-    subprocess.run(self.sp_start + ['Play: {}, {}'.format(tmin, tmax)])
-
-  def pause_sound(self):
-    subprocess.run(self.sp_textgrideditor + ['Play or stop'])
-
-  def stop_sound(self):
-    subprocess.run(self.sp_textgrideditor + ['Interrupt playing'])
-
-  def next_tier():
-    self.remove_log()
-    subprocess.run(self.sp_textgrideditor + ['Select next tier'] + self.sp_write2log)
-
+  def next_tier(self):
+    subprocess.run(self.SENDPRAAT_CMD + ['execute {}'.format(self.NEXT_TIER)])
+    
   def previous_tier(self):
-    self.remove_log()
-    subprocess.run(self.sp_textgrideditor + ['Select previous tier'] + self.sp_write2log)
-
+    subprocess.run(self.SENDPRAAT_CMD + ['execute {}'.format(self.PREVIOUS_TIER)])
+    
   def next_interval(self):
-    self.remove_log()
-    subprocess.run(self.sp_textgrideditor + ['Select next interval'] + self.sp_write2log)
-
+    subprocess.run(self.SENDPRAAT_CMD + ['execute {}'.format(self.NEXT_INTERVAL)])
+    
   def previous_interval(self):
-    self.remove_log()
-    subprocess.run(self.sp_textgrideditor + ['Select previous interval'] + self.sp_write2log)
-
+    subprocess.run(self.SENDPRAAT_CMD + ['execute {}'.format(self.PREVIOUS_INTERVAL)])
+    
   def zoom_all(self):
-    subprocess.run(self.sp_textgrideditor + ['Show all'])
+    subprocess.run(self.SENDPRAAT_CMD + ['execute {}'.format(self.ZOOM_ALL)])
 
   def zoom_in(self):
-    subprocess.run(self.sp_textgrideditor + ['Zoom in'])
+    subprocess.run(self.SENDPRAAT_CMD + ['execute {}'.format(self.ZOOM_IN)])
 
   def zoom_out(self):
-    subprocess.run(self.sp_textgrideditor + ['Zoom out'])
+    subprocess.run(self.SENDPRAAT_CMD + ['execute {}'.format(self.ZOOM_OUT)])
 
-  def zoom_selection(self):
-    subprocess.run(self.sp_textgrideditor + ['Zoom to selection'])
+  def zoom_to_selection(self):
+    subprocess.run(self.SENDPRAAT_CMD + ['execute {}'.format(self.ZOOM_TO_SELECTION)])
 
   def zoom_back(self):
-    subprocess.run(self.sp_textgrideditor + ['Zoom back'])
+    subprocess.run(self.SENDPRAAT_CMD + ['execute {}'.format(self.ZOOM_BACK)])
 
   def push_interval_text(self, text):
-    subprocess.run(self.sp_textgrideditor + [
-    'info$ = Editor info',
-    'tmin = extractNumber(info$, "Selection start: ")',
-    'tmax = extractNumber(info$, "Selection end: ")',
-    'selected_tier = extractNumber(info$, "Selected tier:")',
-    'tmid = (tmin + tmax)*0.5',
-    'endeditor',
-    'selectObject: tg',
-    'interval = Get interval at time: selected_tier, tmid',
-    'Set interval text: selected_tier, interval, "{}"'.format(text),
-    'editor: tg'
-    ])
+    subprocess.run(self.SENDPRAAT_CMD + ['execute {} {}'.format(self.PUSH_TEXT, text)])
 
   def pull_interval_text(self):
-    self.write_editor2log()
-    while True:
-      if os.path.exists(self._logfile):
-        editor = self.load_editor_info()
-        break
-      time.sleep(0.1)
-    return editor['Text']
-    
-  def extract_audio_file(self):
-    self.remove_tempaudio()
-    subprocess.run(self.sp_textgrideditor + [
-    'info$ = Editor info',
-    'tmin = extractNumber(info$, "Selection start: ")',
-    'tmax = extractNumber(info$, "Selection end: ")',
-    'Save selected sound as WAV file: "{}"'.format(self._audiofile)
-    ])
-
-    while True:
-      if os.path.exists(self._audiofile):
-        break
-      time.sleep(0.1)
-
-  def write_editor2log(self):
-    self.remove_log()
-    subprocess.run(self.sp_textgrideditor + self.sp_write2log)
-
-  def load_editor_info(self):
-    editor_info = dict()
-    encoding = self.detect_encoding(self._logfile)
-    with open(self._logfile, mode = 'r', encoding = encoding) as log:
-      lines = log.readlines()
-
-      for line in lines:
-        line = line.rstrip()
-        if line == '':
-          continue
-
-        key, value = line.split(':', 1)
-        value = value.strip()
-
-        if re.match('^[0-9]', value):
-          value = value.split(' ')[0]
-          if value.isnumeric():
-            value = float(value)
-
-        editor_info[key] = value
-    return editor_info
-
-  def detect_encoding(self, f):
-    """
-    * This function was taken from https://github.com/kylebgorman/textgrid/blob/master/textgrid/textgrid.py
-    All credits correspond to his respective author.
-    This helper method returns the file encoding corresponding to path f.
-    This handles UTF-8, which is itself an ASCII extension, so also ASCII.
-    """
-    encoding = 'ascii'
-    try:
-      with codecs.open(f, 'r', encoding='utf-16') as source:
-        source.readline()  # Read one line to ensure correct encoding
-    except UnicodeError:
-      try:
-        with codecs.open(f, 'r', encoding='utf-8-sig') as source: #revised utf-8 to utf-8-sig for utf-8 with byte order mark (BOM)  
-          source.readline()  # Read one line to ensure correct encoding
-      except UnicodeError:
-        with codecs.open(f, 'r', encoding='ascii') as source:
-          source.readline()  # Read one line to ensure correct encoding
-      else:
-        encoding = 'utf-8-sig' #revised utf-8 to utf-8-sig for utf-8 with byte order mark (BOM) 
-    else:
-      encoding = 'utf-16'
-    return encoding
+    subprocess.run(self.SENDPRAAT_CMD + ['execute {} {}'.format(self.PULL_TEXT, self.LOGFILE_DIR)])
+    with open(self.LOGFILE_DIR, encoding = 'utf-8') as f:
+      log = f.readline()
+    return log
+ 
+  def extract_audio_file(self, speed):
+    subprocess.run(self.SENDPRAAT_CMD + ['execute {} {} {}'.format(self.EXTRACT_AUDIOFILE, self.AUDIOFILE_DIR, speed)])
   
 if __name__ == '__main__':
   sp = SendPraat(r'C:\Users\lab\Desktop\textgrid-transcriber\sendpraat.exe')
+  sp.extract_audio_file()
